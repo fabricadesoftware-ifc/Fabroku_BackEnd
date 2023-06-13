4daf0bebcaca3a32504c775a009e9cc849f3b6f7
snow-sr <joaofelipi877@gmail.com>
2023-06-13 11:24:20 -0300

    <fix> Add Docstrings for main functions, refactor the class methods

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from typing import Union
+"""
+Main file for FastApi routes
+"""
 from fastapi import FastAPI
-from modules.ssh.sshConnector import SSHConnector
+from modules.ssh.ssh_connector import SSHConnector
 from modules.server import server
 
 app = FastAPI()
@@ -10,29 +12,44 @@ sshClient = SSHConnector()
 
 @app.get("/")
 def read_root():
+    """
+    Root route
+    """
     return "Hello there! This is the Fabroku Api, for more info about how to use, go to /docs"
 
 
 @app.get("/testSsh")
 def test_ssh_connection():
-    Data = sshClient.testConnection()
+    """Route for testing the SSH connection"""
+    data = sshClient.test_connection()
 
-    return {"data": Data}
+    return {"data": data}
 
 
 @app.post("/generatessh")
 def generate_ssh_connection():
-    Data = sshClient.generateConnection()
-    return {"data": Data}
+    """Route for generating the SSH connection"""
+    data = sshClient.generate_connection()
+    return {"data": data}
 
 
 @app.get("/closeconnection")
 def close_connection():
-    sshClient.closeConnection()
+    """Route for closing the SSH connection"""
+    sshClient.close_connection()
     return "Connection closed"
 
 
 @app.get("/appslist")
 def apps_list():
-    Data = server.appsList()
-    return {"data": Data}
+    """Route for getting the list of apps from dokku"""
+    data = server.apps_list()
+    return {"data": data}
+
+
+@app.post("/runcommand")
+def run_command(command: str):
+    """Route for running a command"""
+    data = sshClient.run_command(command)
+    print(data, "Here")
+    return {"data": data}
