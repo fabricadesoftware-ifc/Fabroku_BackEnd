d3855ad382575e084fd68cd69fadfdea8af9e142
snow-sr <joaofelipi877@gmail.com>
2023-06-13 11:25:05 -0300

    <fix> Added Docstring and types at the AppsList

--- modules/server/server.py
+++ modules/server/server.py
@@ -1,22 +1,29 @@
+"""
+This module will be responsible for the dokku commands
+"""
 # Dokku commands that we will use:
 # dokku apps:list
 # dokku apps:create <app-name>
 # dokku apps:destroy <app-name>
 # dokku apps:rename <app-name> <new-name>
-from modules.ssh.sshConnector import SSHConnector
+from modules.ssh.ssh_connector import SSHConnector
 from shared.clear import Clear
 
 
-def appsList():
+def apps_list() -> dict[str, list[str]]:
+    """Get the list of apps from dokku"""
     clearer = Clear()
     client = SSHConnector()
     # only generate connection if it's not active
-    if not client.verifyConnection():
+    if not client.verify_connection():
         print("Not generating connection")
-        client.generateConnection()
+        client.generate_connection()
 
-    data = client.runCommand("dokku apps:list")
-    data.pop(0)  # Remove the first element, which is an message from dokku
-    data = clearer.clearResponse(data)
+    data: list[str] = client.run_command("dokku apps:list")
 
-    return {"Apps": data}
+    if len(data) > 0:
+        data.pop(0)  # Remove the first element, which is an message from dokku
+        data = clearer.clear_response(data)
+        return {"Apps": data}
+
+    return {"Apps": []}
