6997c8762c68a91329ebf4ca5bd238556fb745c7
snow-sr <joaofelipi877@gmail.com>
2023-06-30 16:13:30 -0300

    <Add> Added python Prisma client and it's first implementation

---
+++ modules/db/db.py
@@ -0,0 +1,20 @@
+"""
+@snow-sr
+This module is used to create a connection to the database
+and create the common methods to use it.
+"""
+from prisma import Prisma
+
+
+def main() -> None:
+    """
+    Conection to the database
+    """
+    db_con = Prisma()
+    db_con.connect()
+
+    db_con.disconnect()
+
+
+if __name__ == "__main__":
+    main()
