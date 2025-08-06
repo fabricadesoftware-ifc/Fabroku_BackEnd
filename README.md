e6af66011c7b7a9d1bb85a4ee7e6336a1ca32735
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-08-06 20:53:49 -0300

    feat: Initialize Django project structure and configuration
    - Updated pyproject.toml to set up Django as the main framework with necessary dependencies.
    - Removed unused Clear class from shared/clear.py.
    - Added initial Django project files including settings, URLs, WSGI, ASGI, and manage.py.
    - Configured allowed hosts and middleware in settings.py.
    - Set up REST framework and JWT authentication settings.
    - Created initial URL routing for the admin interface.
    - Added dotenv support for environment variables.

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # Fabroku
-Project manager for the fábrica.
-
-Made with Python and FastApi
-
-To run this project locally, use this:
-```cmd
-   pdm run uvicorn main:app --reload
-```
-
---
+++ src/django_project/settings.py
@@ -0,0 +1,123 @@
+from pathlib import Path
+import os
+import dj_database_url
+from dotenv import load_dotenv
+from datetime import timedelta
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-nice_key")
+MODE = os.getenv("MODE")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+
+
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [
+    # Subdomínios de fexcompany.me
+    "https://*.fexcompany.me",
+    # Subdomínios de fabricadesoftware.ifc.edu.br
+    "https://*.fabricadesoftware.ifc.edu.br",
+    # Front-end local
+    "http://localhost:8000",
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'django_project.urls'
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Fabroku API",
+    "DESCRIPTION": "API para gerenciamento do Fabroku, incluindo endpoints e documentação.",
+    "VERSION": "1.0.0",
+}
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "src/templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+
+WSGI_APPLICATION = 'django_project.wsgi.application'
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
+    )
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Sao_Paulo'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
