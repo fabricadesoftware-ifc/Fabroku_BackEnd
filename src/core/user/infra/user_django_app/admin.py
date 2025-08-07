93ffba32f035e354e64992fa572c5e0093936f38
BlackVSK <phrod2007@gmail.com>
2025-08-07 00:50:48 -0300

    feat: Adiciona modelo Turma e ajusta o modelo User com campos e opções de exibição

--- src/core/user/infra/user_django_app/admin.py
+++ src/core/user/infra/user_django_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User, Turma
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields= ["name", "email"]
+    list_display = ["name", "email"]
+
+@admin.register(Turma)
+class TurmaAdmin(admin.ModelAdmin):
+    fields = ["turma"]
+    list_display = ["turma", "codigo"]
--- src/core/user/infra/user_django_app/models/user.py
+++ src/core/user/infra/user_django_app/models/user.py
@@ -1,13 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from src.core.user.infra.user_django_app.manager import CustomUserManager
+from django.utils import timezone
 
+class Turma(models.Model):
+    TURMA_CHOICES = [
+        ('1info1','1info1' ),
+        ('1info2','1info2' ),
+        ('1info3','1info3' ),
+        ('2info1','2info1' ),
+        ('2info2','2info2' ),
+        ('2info3','2info3' ),
+        ('3info1','3info1' ),
+        ('3info2','3info2' ),
+        ('3info3','3info3' ),
+        ('Redes1', 'Redes1'),
+        ('Redes3', 'Redes3'),
+        ('Redes5', 'Redes5'),
+        ('BSI1', 'BSI1'),
+        ('BSI3', 'BSI3'),
+        ('BSI5', 'BSI5'),
+        ('BSI7', 'BSI7'),
+    ]
+    
+    turma = models.CharField(max_length=10, choices=TURMA_CHOICES, verbose_name="Turma")
+    codigo = models.CharField(max_length=10, unique=True)
+
+    def save(self, *args, **kwargs):
+        ano_atual = timezone.now().year
+        self.codigo = f"{ano_atual}{self.turma}"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.turma}"
+
+    class Meta:
+        verbose_name = "Turma"
+        verbose_name_plural = "Turmas"
 
 class User(AbstractUser):
     username = None
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True, null=True, blank=True)
     password = models.CharField(max_length=255, null=True, blank=True)
+    # turma = models.ForeignKey(Turma, on_delete=models.CASCADE, related_name="Usuarios", null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["name"]
@@ -16,8 +52,9 @@ class User(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
-        return f"{self.id} - {self.name}"
+        return f"{self.name}"
     
     class Meta:
-        verbose_name = "User"
-        verbose_name_plural = "Users"
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+
