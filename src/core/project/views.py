0761b85bf502c6e7e307062f3841673a147ae005
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-12-23 21:01:19 -0300

    feat: Adiciona autenticação ao ProjectViewSet

--- src/core/project/views.py
+++ src/core/project/views.py
@@ -1,4 +1,5 @@
 from drf_spectacular.utils import extend_schema
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Project
@@ -9,3 +10,13 @@ from .serializers import ProjectSerializer
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retorna apenas os projetos do usuário logado."""
+        return Project.objects.filter(users=self.request.user)
+
+    def perform_create(self, serializer):
+        """Ao criar um projeto, adiciona o usuário atual automaticamente."""
+        project = serializer.save()
+        project.users.add(self.request.user)
