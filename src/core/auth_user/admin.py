d2f0ffed7ff80cd792a1cd5f2d243afee0341a22
jonatasperaza <jonatassilvaperaza@gmail.com>
2025-12-24 04:01:54 -0300

    feat: Refatora UserAdmin para utilizar BaseUserAdmin e remove campo username

--- src/core/auth_user/admin.py
+++ src/core/auth_user/admin.py
@@ -1,13 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from core.auth_user.allowed_emails.admin import *  # noqa: F401, F403
 from core.auth_user.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'username', 'email', 'is_active', 'is_staff', 'date_joined']
-    list_filter = ['is_active', 'is_staff', 'date_joined']
-    search_fields = ['username', 'email']
+class UserAdmin(BaseUserAdmin):
+    """Admin customizado para o modelo User sem senha obrigatória."""
+
+    list_display = ['id', 'email', 'name', 'is_active', 'is_staff', 'is_superuser', 'date_joined']
+    list_filter = ['is_active', 'is_staff', 'is_superuser', 'date_joined']
+    search_fields = ['email', 'name']
     ordering = ['id']
     readonly_fields = ['date_joined', 'last_login']
+
+    # Remove username dos campos (não usamos)
+    ordering = ['email']
+    list_display_links = ['id', 'email']
+
+    # Fieldsets para edição (sem senha obrigatória)
+    fieldsets = (
+        (None, {'fields': ('email', 'name')}),
+        ('Informações Pessoais', {'fields': ('avatar_url', 'git_token')}),
+        (
+            'Permissões',
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            },
+        ),
+        ('Datas', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Fieldsets para criação de novo usuário (sem senha)
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('email', 'name', 'is_active', 'is_staff', 'is_superuser'),
+            },
+        ),
+    )
